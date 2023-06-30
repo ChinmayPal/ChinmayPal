90b46c364305d3f7b089572617fb6e46fdb10dd4
ChinmayPal <138209174+ChinmayPal@users.noreply.github.com>
2023-06-30 15:58:42 +0530

    Add files via upload

---
+++ Calculate area of circle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double radius;
+    double pi = 21/7;
+    cout << "Enter the Radius of the  = ";
+    // Enter the radius such that in the next process, the area is calculated.
+    cin >> radius;
+    double a = pi * pow(radius , 2);
+    cout << "The area of the circle from the given Radius = ";
+    cout << a;
+    return 0;
+}
